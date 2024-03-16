b597770e26fb6fc89e16ade181c41edcc0bb11ad
Arkadiusz Mroczkowski <80561@student.pb.edu.pl>
2024-03-16 01:55:47 +0100

    Filtering out servers based on the quantity of servers

--- communicator/server/views.py
+++ communicator/server/views.py
@@ -16,7 +16,8 @@ class ServerListViewSet(viewsets.ViewSet):
         category = request.query_params.get("category")
         qty = request.query_params.get("qty")
         by_user = request.query_params.get("by_user") == "true"
-        by_server_id = qty = request.query_params.get("by_server_id")
+        by_server_id = request.query_params.get("by_server_id")
+
 
         if by_user or by_server_id and not request.user.is_authentificated:
             raise AuthenticationFailed()
@@ -28,6 +29,7 @@ class ServerListViewSet(viewsets.ViewSet):
             user_id = request.user.id
             self.queryset = self.queryset.filter(member=user_id)
 
+
         if by_server_id:
             try:
                 self.queryset = self.queryset.filter(id=by_server_id)
@@ -38,7 +40,6 @@ class ServerListViewSet(viewsets.ViewSet):
 
         if qty:
             self.queryset = self.queryset[: int(qty)]
-
 
         serializer = ServerSerializer(self.queryset, many=True)
         return Response(serializer.data)
