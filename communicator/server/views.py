d5a3863006bfc6edd9c0ea4c52bdd60575d2eb65
Arkadiusz Mroczkowski <80561@student.pb.edu.pl>
2024-04-22 23:48:36 +0200

    axios config

--- communicator/server/views.py
+++ communicator/server/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from .serializer import ServerSerializer
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError, AuthenticationFailed
+from rest_framework.permissions import IsAuthenticated
 from django.db.models import Count
 from .schema import server_list_docs
 
@@ -11,6 +12,7 @@ from .models import Server
 
 class ServerListViewSet(viewsets.ViewSet):
     queryset = Server.objects.all()
+    permission_classes = [IsAuthenticated]
 
     @server_list_docs
     def list(self, request):
