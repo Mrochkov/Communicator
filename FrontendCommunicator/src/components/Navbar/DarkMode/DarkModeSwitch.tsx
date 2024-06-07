e9b77ab256e01ff18f3978a683e3730542a2993c
Arkadiusz Mroczkowski <80561@student.pb.edu.pl>
2024-06-07 14:35:13 +0200

    Fixes and uvicorn installed

--- FrontendCommunicator/src/components/Navbar/DarkMode/DarkModeSwitch.tsx
+++ FrontendCommunicator/src/components/Navbar/DarkMode/DarkModeSwitch.tsx
@@ -4,22 +4,24 @@ import {IconButton, Typography} from "@mui/material";
 import ToggleOffIcon from "@mui/icons-material/ToggleOff";
 import ToggleOnIcon from "@mui/icons-material/ToggleOn";
 import {ModesContext} from "../../../context/DarkModeContext.tsx";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
 
 const DarkModeSwitch = () => {
     const theme = useTheme();
     const lightMode = useContext(ModesContext);
     return (
         <>
-            <Typography variant="body2" sx={{ textTransform: "capitalize" }}>
-                {theme.palette.mode} mode
-            </Typography>
-            <IconButton sx={{m: 0, p: 0, pl: 2}} onClick={lightMode.toggleDarkMode} color="inherit">
-                {theme.palette.mode === "dark" ? (
-                    <ToggleOffIcon sx={{ fontSize: "2.5rem", p: 0 }} />
-                ) : (
-                    <ToggleOnIcon sx={{ fontSize: "2.5rem" }} />
-                )}
-            </IconButton>
+            <Brightness4Icon sx={{ marginRight: "6px", fontSize: "20px"}} />
+                <Typography variant="body2" sx={{ textTransform: "capitalize" }}>
+                    {theme.palette.mode} mode
+                </Typography>
+                <IconButton sx={{m: 0, p: 0, pl: 2}} onClick={lightMode.toggleDarkMode} color="inherit">
+                    {theme.palette.mode === "dark" ? (
+                        <ToggleOffIcon sx={{ fontSize: "2.5rem", p: 0 }} />
+                    ) : (
+                        <ToggleOnIcon sx={{ fontSize: "2.5rem" }} />
+                    )}
+                </IconButton>
 
 
         </>
