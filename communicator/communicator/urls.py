c238b2faf8ce8e08401c0e1d1d87483d9bcf17b8
Arkadiusz Mroczkowski <80561@student.pb.edu.pl>
2024-08-14 14:54:23 +0200

    Configuring token system to login

--- communicator/communicator/urls.py
+++ communicator/communicator/urls.py
@@ -23,17 +23,23 @@ from django.conf import settings
 from django.conf.urls.static import static
 from chat.consumer import ChatConsumer
 from chat.views import MessageViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = DefaultRouter()
 router.register("api/server/select", ServerListViewSet)
 router.register("api/server/category", CategoryListViewSet)
 router.register("api/messages", MessageViewSet, basename="message")
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/docs/schema/', SpectacularAPIView.as_view(), name="schema"),
     path('api/docs/schema/ui/', SpectacularSwaggerView.as_view()),
-
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + router.urls
 
 websocket_urlpatterns = [path("<str:serverId>/<str:channelId>", ChatConsumer.as_asgi())]
