179dca7aa6b9cbb39c81627e5314d37d34d40b20
Arkadiusz Mroczkowski <80561@student.pb.edu.pl>
2024-10-03 00:59:08 +0200

    saving access tokens in localstorage

--- FrontendCommunicator/src/service/AuthService.ts
+++ FrontendCommunicator/src/service/AuthService.ts
@@ -3,6 +3,38 @@ import axios from "axios";
 
 
 export function useAuthService(): AuthServiceProps {
+
+    const getUserDetails = async () => {
+        try{
+            const userId = localStorage.getItem("userId")
+            const accessToken = localStorage.getItem("access_token")
+            const response = await axios.get(
+                `http://127.0.0.1:8000/api/user/?user_id=${userId}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`
+                    }
+                }
+            );
+            const userDetails = response.data
+            localStorage.setItem("username", userDetails.username);
+
+        } catch (err: any) {
+            return err;
+        }
+    }
+
+    const getUserIdUsingToken = (access : string) => {
+        const token = access
+        const tokenComponents = token.split('.')
+        const encodedPayload = tokenComponents[1]
+        const decodedPayload = atob(encodedPayload)
+        const payloadData = JSON.parse(decodedPayload)
+        const userId = payloadData.user_id
+
+        return userId;
+    }
+
     const login = async (username: string, password: string) => {
         try{
             const response = await axios.post(
@@ -11,7 +43,15 @@ export function useAuthService(): AuthServiceProps {
                     password,
                 }
             );
-            console.log(response);
+
+            const {access, refresh} = response.data;
+
+            localStorage.setItem("access_token", access);
+            localStorage.setItem("refresh_token", refresh);
+            localStorage.setItem("UserId", getUserIdUsingToken(access))
+
+            getUserDetails()
+
         } catch (err: any) {
             return err;
         }
--- communicator/account/views.py
+++ communicator/account/views.py
@@ -3,10 +3,12 @@ from rest_framework.response import Response
 from .models import Account
 from .serializers import UserSerializer
 from .schemas import user_list_docs
+from rest_framework.permissions import IsAuthenticated
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = Account.objects.all()
+    permission_classes = [IsAuthenticated]
 
     @user_list_docs
     def list(self, request):
