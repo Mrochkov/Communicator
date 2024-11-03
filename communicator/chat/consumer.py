190486d621df9bb42f4bb55e8789d4c29580fdc4
Arkadiusz Mroczkowski <80561@student.pb.edu.pl>
2024-11-03 01:54:51 +0100

    more fixes

--- communicator/chat/consumer.py
+++ communicator/chat/consumer.py
@@ -25,43 +25,25 @@ class ChatConsumer(JsonWebsocketConsumer):
             self.is_member = is_member
 
     def connect(self):
-        try:
-            self.user = self.scope["user"]
-            self.channel_id = self.scope["url_route"]["kwargs"]["channelId"]
-            self.accept()
 
-            if not self.user.is_authenticated:
-                self.close(code=4001)
-                return
+        self.user = self.scope["user"]
+        self.accept()
+        if not self.user.is_authenticated:
+            print(self.user)
+            self.close(code=4001)
 
-            try:
-                server = Server.objects.get(id=self.channel_id)
-            except Server.DoesNotExist:
-                print(f"Server with ID {self.channel_id} does not exist.")
-                self.close(code=4004)
-                return
+        # Called on connection.
+        # To accept the connection call:
+        self.channel_id = self.scope["url_route"]["kwargs"]["channelId"]
 
-            self.is_member = server.member.filter(id=self.user.id).exists()
-            if not self.is_member:
-                print(f"User {self.user.id} is not a member of server {self.channel_id}")
-                self.close(code=4003)
-                return
+        self.user = User.objects.get(id=self.user.id)
 
-            async_to_sync(self.channel_layer.group_add)(
-                f"server_{self.channel_id}", self.channel_name
-            )
-
-        except Exception as e:
-            print(f"Error during WebSocket connection: {e}")
-            self.close(code=1011)
+        async_to_sync(self.channel_layer.group_add)(
+            self.channel_id,
+            self.channel_name
+        )
 
     def receive_json(self, content):
-
-        if not self.is_member:
-            self.send_json({"error": "You are not a member of this server"})
-            print("User is not a member; closing connection.")
-            self.close(code=4003)
-            return
 
         channel_id = self.channel_id
         sender_id = self.user
